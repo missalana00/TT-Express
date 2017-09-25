becfc170728b49861e68552422e631d403c4c0ce
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-09-25 09:11:12 -0500

    Reviewing pug

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 
 // Require the express module and assigned it to variable 'express'
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+  ];
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send("<h1>Here it is, yes.</h1>");
+    // res.send("<h1>Here it is, yes.</h1>");
+    res.render('index');
+});
+
+app.get('/cards', (req, res) => {
+    res.render("card", { prompt: "Who is buried in Grant's tomb?" });
+    // res.locals.prompt = "Who is buried in Grant's tomb?"
+    // res.render('card');
 });
 
 app.get('/hello', (req, res) => {
-    res.send("<h1>Hello there.</h1>");
+    res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+    console.dir(req.body);
+    res.render('hello');
 });
 
 app.listen(3000, () => {
